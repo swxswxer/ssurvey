f01697e9112de676fa88799341f6d61d83c49abe
swxswx <a913685201@163.com>
2025-05-15 21:12:01 +0800

    更改前端请求时限60s->120s

--- ssurvey-frontend/src/request.ts
+++ ssurvey-frontend/src/request.ts
@@ -3,7 +3,7 @@ import { Message } from "@arco-design/web-vue";
 
 const myAxios = axios.create({
   baseURL: "http://localhost:8101",
-  timeout: 60000,
+  timeout: 120000,
   withCredentials: true,
 });
 
